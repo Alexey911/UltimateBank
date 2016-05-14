9ca7a95dd6b87a3733f02096e13f5d9df2679a56
Alexey911 <alexeyzhytnik@gmail.com>
2016-05-14 07:05:21 +0300

    Update scripts

--- src/main/resources/db/ddm/I-10.sql
+++ src/main/resources/db/ddm/I-10.sql
@@ -1,14 +1,12 @@
 INSERT INTO CREDITCARD (ID,
-                      CODE,
-                      PASSWORD,
-                      VALIDITY,
-                      CVC,
-                      VALIDATIONCODE,
-                      CREDIT)
+                        CODE,
+                        VALIDITY,
+                        CVC,
+                        VALIDATIONCODE,
+                        CREDIT)
 VALUES (
   1,
   'My card',
-  'pass',
   '123',
   1234,
   1,
